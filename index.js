bb07628df445c923b23472faabd51b8e694dadaf
Prashant Verma <prashantvermaiiitb@gmail.com>
2022-07-10 13:14:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import { wizard } from './wizard';
+import { barbarian } from './barbarian';
+
+// const wizard = require('./wizard');
+// const barbarian = require ('./barbarian');
+
+console.log('Ran from index.js of webpack setup');
+console.log(wizard);
+console.log(barbarian);
